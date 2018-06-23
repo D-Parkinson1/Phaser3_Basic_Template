e6a0849d22cbf69199c628fd4c65a7875aeedf04
Daniel <24710205+Fabadiculous@users.noreply.github.com>
2018-06-23 17:28:19 +1000

    Initial commit

--- src/preload.js
+++ src/preload.js
@@ -22,47 +22,17 @@ class Preload extends Phaser.Scene {
         this.load.setPath('/assets/images/');
 
 
-        this.load.image([
-            { key: 'clouds', url: 'Clouds.png'},
-            { key: 'sky', url: 'Sky.png'},
-            { key: 'spike', url: 'Spike.png'},
-            { key: 'pauseMenu', url: 'PauseMenu.png'},
-            { key: 'door', url: 'Door.png'}
-        ]);
+        this.load.image([ ]);
 
-        this.load.spritesheet([
-            { key: 'button', url: 'Button.png', frameConfig: {frameWidth: 200, frameHeight: 50} },
-            { key: 'coin', url: 'Coin.png', frameConfig: {frameWidth: 16, frameHeight: 16} },
-            { key: 'snake', url: 'Enemy.png', frameConfig: {frameWidth: 32, frameHeight: 32} },
-            { key: 'flyEnemy', url: 'FlyingEnemy.png', frameConfig: {frameWidth: 25, frameHeight: 23} },
-            { key: 'movingPlatform', url: 'MovingPlatform.png', frameConfig: {frameWidth: 64, frameHeight: 32} },
-            { key: 'player', url: 'Player.png', frameConfig: {frameWidth: 16, frameHeight: 32} },
-            { key: 'groundTiles', url: 'GroundTiles.png', frameConfig: {frameWidth: 64, frameHeight: 64} },
-            { key: 'soundBtn', url: 'SoundButtons.png', frameConfig: {frameWidth: 64, frameHeight: 64} },
-            { key: 'star', url: 'Stars.png', frameConfig: {frameWidth: 20, frameHeight: 20} },
-            { key: 'lvlThumbnail', url: 'Thumbnail.png', frameConfig: {frameWidth: 64, frameHeight: 64} }
-        ]);
+        this.load.spritesheet([ ]);
 
         // Path for audio
         this.load.setPath('assets/sounds');
 
-        this.load.audio('getStar', [ 'getStar.mp3', 'getStar.ogg' ]);
-
-        // Path for levels
-        this.load.setPath('assets/levels');
-        this.load.tilemapTiledJSON('tutorial', 'tutorial.json');
-        for (let i = 1; i < 10; i++) {
-            this.load.tilemapTiledJSON(`level${i}`, `Level${i}.json`);
-        }
+        this.load.audio();
     }
 
     create () {
-        this.registry.set('width', this.sys.game.config.width);
-        this.registry.set('height', this.sys.game.config.height);
-        this.scene.launch('background');
-        this.scene.launch('createAnims');
-        this.scene.launch('loadLevels');
-        this.scene.launch('menuUI', { title: 'SPEEDRUNNER', backBtn: false });
         this.scene.start('menu');
     }
 
--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
     },
+    externals: { phaser: 'phaser' },
     module: {
         rules: [
             {
